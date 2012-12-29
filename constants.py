82c65b9fa33a661e4c75cb34ab22ce507e8cbe94
Joshua Blum <joshblum@mit.edu>
2012-12-29 14:36:29 -0500

    relative path

--- constants.py
+++ constants.py
@@ -1,2 +1,6 @@
-WORDS_FILE = 'en-dict.txt'
-DICT_FILE = 'en-dict.pkl'
+import os
+
+PATH = os.path.dirname(__file__)
+
+WORDS_FILE = os.path.join(PATH, 'en-dict.txt')
+DICT_FILE = os.path.join(PATH, 'en-dict.pkl')
